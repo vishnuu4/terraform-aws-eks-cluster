c3f4ba5738c7792145b0d54bd02bf07dfd16d2ce
Vishnu Angara <vishnuvardhanangara@gmail.com>
2026-06-11 16:34:17 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,9 +7,53 @@ data "aws_availability_zones" "available" {}
 resource "random_string" "suffix" {
   length  = 6
   special = false
-  upper   = false
 }
 
 locals {
   cluster_name = "${var.cluster_name}-${random_string.suffix.result}"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.19"
+
+  name = "eks-vpc"
+
+  cidr = var.vpc_cidr
+
+  azs = slice(
+    data.aws_availability_zones.available.names,
+    0,
+    2
+  )
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = {
+    Environment = "Dev"
+    Terraform   = "true"
+    Project     = "EKS"
+  }
 }
