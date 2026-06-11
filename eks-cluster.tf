0a396acb72e04cfa18fdad16f1f41119dc725dc4
Vishnu Angara <vishnuvardhanangara@gmail.com>
2026-06-11 16:31:49 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,7 +1,22 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "~> 20.37"
 
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    node_group = {
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+    }
+  }
 }
