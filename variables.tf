642c2bf6a4366dace3ed93bfc108eeda19fa7d5b
Vishnu Angara <vishnuvardhanangara@gmail.com>
2026-06-11 16:30:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,15 @@
 variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "ap-south-1"
+  default = "ap-south-1"
 }
 
 variable "cluster_name" {
-  description = "EKS Cluster Name"
-  type        = string
-  default     = "vishnu-eks-cluster"
+  default = "vishnu-eks-cluster"
 }
 
 variable "kubernetes_version" {
-  description = "Kubernetes Version"
-  type        = string
-  default     = "1.33"
+  default = "1.33"
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR Block"
-  type        = string
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
